384c6674a6b83aa8916f4f9fc31ce353a43061b2
Gerald Saberon <79496236+geraldsaberon@users.noreply.github.com>
2024-11-21 19:53:10 +0800

    Add light/dark theme toggle

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,8 +5,18 @@ export default {
         extend: {
             colors: {
                 main: "#22c55e",
+            },
+            animation: {
+                blink: "blink 1.5s infinite step-end"
+            },
+            keyframes: {
+                blink: {
+                    "from, to": {opacity: 1},
+                    "50%": {opacity: 0}
+                }
             }
         },
     },
+    darkMode: 'selector',
     plugins: [],
 }
